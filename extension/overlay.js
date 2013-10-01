3d6f2c0e6ccf2842316696082e6e0439b955d3fe
BigRomanov <big.romanov@gmail.com>
2013-10-01 23:15:41 +0300

    Fixed some formatting issues

--- extension/overlay.js
+++ extension/overlay.js
@@ -1,13 +1,8 @@
 console.log("ShareWiz overlay script running");
-
-
 chrome.extension.sendMessage({enabled: "enabled"}, function(response) {
   // check the response
   if(response.enabled == "true") {
     console.log("ShareWiz Enabled: injecting overlay");
-    $('body').append('
-      <div id="sharewizFooter">THIS IS THE DIV </div>
-    ');
+    $('body').append('<div id="sharewizFooter">THIS IS THE DIV </div>');
   }
-});
-
+});     
